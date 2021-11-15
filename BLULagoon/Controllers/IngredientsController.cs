7cd37f0974f0385e38b2722aa423bbaba185dd10
lukas9415 <lukas.liutkevicius4@gmail.com>
2021-11-15 13:53:06 +0200

    Documentation part 1

--- BLULagoon/Controllers/IngredientsController.cs
+++ BLULagoon/Controllers/IngredientsController.cs
@@ -5,18 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BLULagoon.Controllers
 {
+    /// <summary>
+    /// Controller for ingredient requests.
+    /// </summary>
     [Route("BLULagoon/[controller]")]
     [ApiController]
     public class IngredientsController : ControllerBase
     {
         IngredientRepository ingredientRepository = new IngredientRepository();
 
+        /// <summary>
+        /// Request to get all ingredients from the database.
+        /// 
+        /// <c>GET: BLULagoon/Ingredients</c>
+        /// </summary>
+        /// <returns>Returns all existing ingredients</returns>
         [HttpGet]
         public List<Ingredient> Get()
         {
             return IngredientRepository.GetAllIngredients();
         }
 
+        /// <summary>
+        /// Request to add a new ingredient to the database.
+        /// 
+        /// <c>POST: BLULagoon/Ingredients</c>
+        /// </summary>
+        /// <param name="ingredientName">New ingredient name which comes from the request</param>
+        /// <returns>Returns all existing ingredients with the new one</returns>
         [HttpPost]
         public List<Ingredient> PostNewIngredient(string ingredientName)
         {
@@ -25,6 +41,14 @@ namespace BLULagoon.Controllers
             return Get();
         }
 
+
+        /// <summary>
+        /// Request to delete an ingredient from the database.
+        /// 
+        /// <c>DELETE: BLULagoon/Ingredients</c>
+        /// </summary>
+        /// <param name="ingredientID">ingredientID which comes from the request</param>
+        /// <returns>Returns all ingredients after deletion</returns>
         [HttpDelete("{ingredientID}")]
         public List<Ingredient> Delete(int ingredientID)
         {
@@ -33,6 +57,14 @@ namespace BLULagoon.Controllers
             return Get();
         }
 
+        /// <summary>
+        /// Request to update an ingredient from the database.
+        /// 
+        /// <c>PUT: BLULagoon/Ingredients</c>
+        /// </summary>
+        /// <param name="ingredientID">ingredientID which comes from the request</param>
+        /// <param name="ingredientName">name which the ingredient will be updated to</param>
+        /// <returns>Returns information about the updated ingredient</returns>
         [HttpPut]
         public string Put(int ingredientID, string ingredientName)
         {
--- BLULagoon/Controllers/UsersController.cs
+++ BLULagoon/Controllers/UsersController.cs
@@ -5,20 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BLULagoon.Controllers
 {
+    /// <summary>
+    /// Controller for user requests.
+    /// </summary>
     [Route("BLULagoon/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
         UsersRepository repository = new UsersRepository();
-
-
+        
+        /// <summary>
+        /// Request to get all users from the database.
+        /// 
+        /// <c>GET: BLULagoon/Users (All users)</c>
+        /// </summary>
+        /// <returns>Returns all existing users</returns>
         [HttpGet]
         public List<User> Get()
         {
             return UsersRepository.GetAllUsers();
         }
 
-
+        /// <summary>
+        /// Request to add a new user to the database.
+        /// 
+        /// <c>POST: BLULagoon/Users</c>
+        /// </summary>
+        /// <param name="newUser">newUser object which comes from the request with required information</param>
+        /// <returns>Returns the added user</returns>
         [HttpPost]
         public User PostNewUser([FromBody] User newUser)
         {
@@ -27,7 +41,13 @@ namespace BLULagoon.Controllers
             return newUser;
         }
 
-
+        /// <summary>
+        /// Request to delete a user from the database.
+        /// 
+        /// <c>DELETE: BLULagoon/Users/{cryptoId}</c>
+        /// </summary>
+        /// <param name="userid">userid which comes from the request</param>
+        /// <returns>Returns all users after deletion</returns>
         [HttpDelete("{userid}")]
         public List<User> Delete(int userid)
         {
